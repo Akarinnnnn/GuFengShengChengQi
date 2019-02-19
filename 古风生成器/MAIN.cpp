c47d4e22c6016149a22e41c56eb0d5f983a03532
Akarinnnnn <1207247008@qq.com>
2019-02-19 22:29:31 +0800

    加点东西

---
+++ 古风生成器/MAIN.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+//能编译
+//"aaa,aaa bb了bb" 等
+using namespace std;
+typedef vector<wstring> 词典;
+词典 四字表;
+词典 两字表;
+词典 句式;
+
+wstring 四字()
+{
+
+}
+
+wstring 两字()
+{
+
+}
+
+wstring 句子()
+{
+
+}
+
+int wmain()
+{
+	wcout.imbue(locale("chs"));
+	wifstream dict2(L".\\两字.txt");
+	wifstream dict4(L".\\四字.txt");
+	wifstream stances(L".\\句子");
+	
+}
